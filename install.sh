4ed7f0d29f846ceb162e2321ce196521b6f87579
Mushan Yang <mushanyoung@gmail.com>
2025-03-05 14:14:57 -0800

    ravy: update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Dependencies: git, vim, fzf, fd, curl
+# Dependencies for Linux: build-essential
+
 SHELL=/bin/sh
 
 set -e
@@ -9,79 +12,12 @@ __el() { echo "> $@" ; "$@" ; }
 
 __banner () { echo; echo "===== $@"; }
 
-# link_homebrew () {
-#   for brew in "$HOME/.brew" "$HOME/.linuxbrew" "/home/linuxbrew/.linuxbrew" "/usr/local"; do
-#     test -f "$brew/bin/brew" && eval "$($brew/bin/brew shellenv)"
-#   done
-#   true
-# }
-
 append_content_if_absent () {
   file="$1" text="$2" appending=${3:-$2}
   if ! grep "$text" "$file" >/dev/null 2>&1; then
     [ -f "$appending" ] && cat "$appending" >> "$file" || echo "$appending" >> "$file"
   fi
 }
-
-# if [ $(uname) = Linux ]; then
-#   __banner Linux packages
-#   if type apt-get >/dev/null 2>&1; then
-#     deps_to_install=""
-#     for dep in "build-essential" "curl" "file" "git"; do
-#       echo -n "Checking apt for $dep... "
-#       if ! type apt >/dev/null 2>&1 || \
-#          ! apt -qq list $dep 2>/dev/null | grep -E "installed|upgradable" >/dev/null; then
-#         deps_to_install="$deps_to_install $dep"
-#         echo "to install"
-#       else
-#         echo "found"
-#       fi
-#     done
-#     if [ -n "$deps_to_install" ]; then
-#       __el sudo apt-get update
-#       __el sudo apt-get install -y ${deps_to_install}
-#     fi
-#   elif type yum >/dev/null 2>&1; then
-#     __el sudo yum groupinstall -y 'Development Tools'
-#     __el sudo yum install -y curl file git libxcrypt-compat
-#   else
-#     echo "No supported package manager is found."
-#     echo "Please manually install Linuxbrew dependencies."
-#   fi
-# fi
-
-# __banner Homebrew
-
-# link_homebrew
-
-# if ! type brew >/dev/null 2>&1; then
-#   echo "Homebrew / Linuxbrew is not present."
-#   if [ $(uname) = Darwin ]; then
-#     echo "Installing Homebrew..."
-#     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#   elif [ $(uname) = Linux ]; then
-#     echo "Installing Linuxbrew..."
-#     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-#   else
-#     echo "The system is not standard Linux or OSX."
-#     echo "Please manually install Homebrew / Linuxbrew and rerun the script."
-#     exit
-#   fi
-
-#   link_homebrew
-#   if ! type brew >/dev/null 2>&1; then
-#     echo "Homebrew / Linuxbrew can not be loaded. Exiting."
-#     exit
-#   fi
-# fi
-
-# __banner Homebrew formulae
-
-# for dep in "git" "vim" "fzf" "fd"; do
-#   if ! type $dep >/dev/null 2>&1; then
-#     __el brew install $dep
-#   fi
-# done
 
 __banner mkdir
 __el mkdir -p $HOME/.config $HOME/.config/fish $HOME/.config/fish/functions $HOME/.vim $HOME/.vim/bundle $HOME/.vim/tmp $HOME/.vim/autoload
@@ -130,8 +66,8 @@ __el curl -sfLo $HOME/.tmux.conf https://raw.githubusercontent.com/gpakosz/.tmux
 __el ln -s -f $RAVY/tmux.conf.local $HOME/.tmux.conf.local
 
 __banner custom
-if command -v "$RAVY/custom/install" >/dev/null; then
-  "$RAVY/custom/install" || true
+if command -v "$RAVY/custom/install.sh" >/dev/null; then
+  "$RAVY/custom/install.sh" || true
 fi
 
 __banner complete
